ffab7ba8111eb62540a97fd5b44218fbe6ea6308
Marisua <31741111+GreenhillTeacher@users.noreply.github.com>
2022-06-07 10:39:37 -0500

    de

---
+++ fileStuff.py
@@ -0,0 +1,30 @@
+#Maria Suarez
+# Files"
+# r read
+# w write
+# a append
+# 
+# open files make sure you close the file
+
+import os, datetime
+os.system('cls')
+name="Maria"
+score=120
+date=datetime.datetime.now() #todays date and time
+print(date.strftime("%m/%d/%Y"))
+print(date.strftime("%Y   %m   %d"))
+screLine= str(score) + "\t"+name+"\t"+ date.strftime("%m/%d/%Y")
+print(screLine)
+#To  open a file we must create an object
+myFile=open("scre.txt", 'w') #open a file to write it clear the file if it exists
+myFile.write(screLine)
+myFile.close()
+myFile=open("scre.txt", 'a') #open a file to write it clear the file if it exists
+myFile.write(screLine)
+myFile.close()
+myFile=open("scre.txt", 'r') #open a file to read
+#lines=myFile.readline()
+print()
+for line in myFile.readlines():
+    print(line)
+myFile.close()
--- wdGame.py
+++ wdGame.py
@@ -10,9 +10,12 @@ os.system ('cls')
 from time import sleep
 seconds=.5
 
+theWord=""
 
+list1 = ["coral","scallop","sea urchin","oyster","mussel","cockle","clam","geoduck","abelone","ostrea"]
+list2 = ["apple","kiwi",'Banana']
+list3 = ["CPU","RAM", "ROM"]
 
-list = ["coral","scallop","sea urchin","oyster","mussel","cockle","clam","geoduck","abelone","ostrea"]
 Game=True
 cnt=0
 #a function is a section  the prram that we call when we need it
@@ -41,8 +44,19 @@ def hint():
         print("You are horrible at guessing, no more hints, go till you get it right")
     
     print()
+def selectWrd(choice):  #is a function with a parameter
+    global theWord
+    if choice ==1:
+        theWord= random.choice(list1)    
+    if choice ==2:
+        theWord= random.choice(list2)
+    if choice ==3:
+        theWord= random.choice(list3)
+    return theWord  
+name=input("What is your name? ")
+high=0 #tfind highest score
 while Game:
-    theword=random.choice(list)
+    
     print("|***************************************|")
     print("|         Guessing  Game   !!           |")
     print("|^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^|")
@@ -54,7 +68,7 @@ while Game:
     print("|  These animals are big fans of water  |")
     print("|***************************************|")
 # add user name, make it more personal y will need for keeping score
-    name=input("What is your name? ")
+    
     print(name, end=", ")
     answer=input("Would you like to play? ")
     if 'n' in answer:
@@ -70,24 +84,35 @@ while Game:
                 print("give me 1,2  3")
         except:
             print("sorry")
+    theWord = selectWrd(choice) #call to a function that will return a value
     #call function to select the word from the right list
     os.system('cls')
     check=True
     while check and cnt <5:
         guess=input("plese put your guess here: ")
         print()
-        if guess == theword:
+        if guess == theWord:
             print("Congrats, You got it")
             check=False
         else:
             hint()
         cnt+=1   # cnt= cnt + 1
+        if cnt ==5:
+            print("Sorry!" )
+    score=200-40*cnt
+    if score > high:   # find highest sce
+        high=score
+    print(name+", your score is "+str(score))
+    input("Press enter ")
     os.system('cls')
     print("<><><><><><><><><><><><>")
     answer=input("Do yo want to play again? ")
     if ('n' or 'N') in answer:
         Game=False
         print("Thank you for playing my game" )
+    
+    cnt=0 
+print("your highest score is " + str(high))
 
 
 
