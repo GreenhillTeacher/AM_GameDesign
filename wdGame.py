3390ee8bc3bafece14bc4f0694b9e0d690847fa5
Marisua <31741111+GreenhillTeacher@users.noreply.github.com>
2022-06-07 10:51:13 -0500

    update

--- wdGame.py
+++ wdGame.py
@@ -13,8 +13,7 @@ seconds=.5
 theWord=""
 
 list1 = ["coral","scallop","sea urchin","oyster","mussel","cockle","clam","geoduck","abelone","ostrea"]
-list2 = ["apple","kiwi",'Banana']
-list3 = ["CPU","RAM", "ROM"]
+
 
 Game=True
 cnt=0
@@ -44,17 +43,7 @@ def hint():
         print("You are horrible at guessing, no more hints, go till you get it right")
     
     print()
-def selectWrd(choice):  #is a function with a parameter
-    global theWord
-    if choice ==1:
-        theWord= random.choice(list1)    
-    if choice ==2:
-        theWord= random.choice(list2)
-    if choice ==3:
-        theWord= random.choice(list3)
-    return theWord  
-name=input("What is your name? ")
-high=0 #tfind highest score
+
 while Game:
     
     print("|***************************************|")
@@ -68,9 +57,10 @@ while Game:
     print("|  These animals are big fans of water  |")
     print("|***************************************|")
 # add user name, make it more personal y will need for keeping score
-    
+    name=input("What is your name? ")
     print(name, end=", ")
     answer=input("Would you like to play? ")
+    theWord=random.choice(list1)
     if 'n' in answer:
         break
     while True:
@@ -84,7 +74,6 @@ while Game:
                 print("give me 1,2  3")
         except:
             print("sorry")
-    theWord = selectWrd(choice) #call to a function that will return a value
     #call function to select the word from the right list
     os.system('cls')
     check=True
@@ -97,22 +86,12 @@ while Game:
         else:
             hint()
         cnt+=1   # cnt= cnt + 1
-        if cnt ==5:
-            print("Sorry!" )
-    score=200-40*cnt
-    if score > high:   # find highest sce
-        high=score
-    print(name+", your score is "+str(score))
-    input("Press enter ")
     os.system('cls')
     print("<><><><><><><><><><><><>")
     answer=input("Do yo want to play again? ")
     if ('n' or 'N') in answer:
         Game=False
         print("Thank you for playing my game" )
-    
-    cnt=0 
-print("your highest score is " + str(high))
 
 
 
